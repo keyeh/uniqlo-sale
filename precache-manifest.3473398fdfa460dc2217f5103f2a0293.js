551cde276af7ca36fcaa4bcb15cb648f55fde5eb
keyeh <keyeh@users.noreply.github.com>
2019-03-01 14:53:49 -0800

    Updates

---
+++ precache-manifest.3473398fdfa460dc2217f5103f2a0293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf1e76e4ede0972cdb8d",
+    "url": "/uniqlo-sale/static/js/runtime~main.cf1e76e4.js"
+  },
+  {
+    "revision": "1da0a54a746d5c8290db",
+    "url": "/uniqlo-sale/static/js/main.1da0a54a.chunk.js"
+  },
+  {
+    "revision": "8da7081e3c3e78cb710c",
+    "url": "/uniqlo-sale/static/js/2.8da7081e.chunk.js"
+  },
+  {
+    "revision": "1da0a54a746d5c8290db",
+    "url": "/uniqlo-sale/static/css/main.e80cb99d.chunk.css"
+  },
+  {
+    "revision": "fdf22212d52ed6bc44694f37364a48e9",
+    "url": "/uniqlo-sale/index.html"
+  }
+];
